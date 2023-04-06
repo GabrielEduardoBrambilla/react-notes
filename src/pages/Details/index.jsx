0e89a9a4a69f5658354cabdd975dc3d8421b97c6
Brambs <gabriel128.br@gmail.com>
2023-04-06 08:03:45 -0300

    Components

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,28 @@
-import { Container } from './styles'
+import { Container, Links } from './styles'
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 
 
 export function Details() {
   return (
     <Container>
       <Header />
-      <h1>Hello world</h1>
-      <Button />
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="#">sadsadsad</a>
+          </li>
+          <li><a href="">okoko</a></li>
+        </Links>
+      </Section>
+      <Section title="Marcadores">
+        <Tag title='Express' />
+        <Tag title='Node' />
+
+      </Section>
+      <Button title='Enviar' />
     </Container>
 
   )
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -10,3 +10,17 @@ export const Container = styled.div`
     'header'
     'content';
 `
+export const Links = styled.ul`
+  list-style: none;
+
+  > li {
+    margin-top: 12px;
+
+    a {
+      color: ${({ theme }) => theme.COLORS.WHITE};
+      &:visited {
+        color: ${({ theme }) => theme.COLORS.WHITE};
+      }
+    }
+  }
+`
