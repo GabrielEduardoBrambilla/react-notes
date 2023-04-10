5a8a34755b2fd3fe766e57a2d81f1db5a5159818
Brambs <gabriel128.br@gmail.com>
2023-04-10 19:53:25 -0300

    Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,26 @@
 import { Header } from '../../components/Header'
+import { ButtonText } from '../../components/ButtonText'
+
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
 export function Home() {
   return (
     <Container>
       <Brand>
-        <h1>Home</h1>
+        <h1>React Notes</h1>
       </Brand>
       <Header></Header>
-      <Menu></Menu>
+      <Menu>
+        <li>
+          <ButtonText title="Todos"></ButtonText>
+        </li>
+        <li>
+          <ButtonText title="React"></ButtonText>
+        </li>
+        <li>
+          <ButtonText title="NodeJS"></ButtonText>
+        </li>
+      </Menu>
       <Search></Search>
       <Content></Content>
       <NewNote></NewNote>
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: grid;
+  grid-template-columns: 250px auto;
+  grid-template-rows: 105px 128px auto 64px;
+  grid-template-areas:
+    'brand header'
+    'menu search'
+    'menu content'
+    'newnote content';
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+`
+export const Brand = styled.div`
+  grid-area: brand;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+  > h1 {
+    font-size: 24px;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+`
+export const Menu = styled.ul`
+  grid-area: menu;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+`
+export const Search = styled.view``
+export const Content = styled.view``
+export const NewNote = styled.view``
