0727f640058841d5ebc864aef1d5ba75f2d1c265
Brambs <gabriel128.br@gmail.com>
2023-04-20 10:02:08 -0300

    test

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,69 +1,80 @@
 import { useState } from "react";
-import { Container, Form, Background } from "./styles";
-import { Input } from "../../components/Input";
-import { Button } from "../../components/Button";
-import { FiLock, FiLogIn, FiMail, FiUser } from "react-icons/fi";
+import { FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Link, useNavigate } from 'react-router-dom';
 
 import { api } from "../../services/api";
 
+import { Input } from "../../components/Input";
+import { Button } from '../../components/Button';
+
+import { Container, Form, Background } from "./styles";
+
 export function SignUp() {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  function handleSingUp() {
+  function handleSignUp() {
     if (!name || !email || !password) {
-      alert("Please enter name email and password ")
+      return alert("Preencha todos os campos!");
     }
 
-    api.post("/users", { name, email, password }).then(() => {
-      alert("Usuario cadastrado com sucesso")
-      navigated("/")
-    }).catch(error => {
-      if (error.response) {
-        alert(error.response.data.message)
-      } else {
-        alert("Nao foi possivel cadastrar")
-      }
-    })
-
+    api.post("/users", { name, email, password })
+      .then(() => {
+        alert("Cadastro realizado com sucesso!");
+        navigate("/");
+      }).catch(error => {
+        if (error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("Não foi possível cadastrar.");
+        }
+      });
   }
 
   return (
     <Container>
       <Background />
+
       <Form>
-        <h1>React Notes</h1>
-        <p>Aplicação para salvar e gerenciar links uteis</p>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
         <h2>Crie sua conta</h2>
+
         <Input
           placeholder="Nome"
           type="text"
           icon={FiUser}
-          onChange={e => setEmail(e.target.value)}
+          onChange={e => setName(e.target.value)}
         />
+
         <Input
           placeholder="E-mail"
           type="text"
           icon={FiMail}
           onChange={e => setEmail(e.target.value)}
         />
+
         <Input
-          placeholder="*****"
+          placeholder="Senha"
           type="password"
           icon={FiLock}
           onChange={e => setPassword(e.target.value)}
         />
-        <Button title="Cadastrar" onClick={handleSingUp} />
+
+        <Button
+          title="Cadastrar"
+          onClick={handleSignUp}
+        />
 
         <Link to="/">
           Voltar para o login
         </Link>
       </Form>
+
     </Container>
   )
 }
